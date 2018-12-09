3ac0b9a458a00c957b1bdbf69c386dc62c8580ab
rafapessoa <rafa.pessoa1@gmail.com>
2018-12-09 19:49:06 -0200

    Add Save user preferences

--- app/src/main/java/com/example/rafael/calculaflex/FormActivity.kt
+++ app/src/main/java/com/example/rafael/calculaflex/FormActivity.kt
@@ -1,13 +1,19 @@
 package com.example.rafael.calculaflex
 
+import android.content.Context
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.rafael.calculaflex.extensions.format
 import com.example.rafael.calculaflex.extensions.getDouble
 import com.example.rafael.calculaflex.watchers.DecimalTextWatcher
 import kotlinx.android.synthetic.main.activity_form.*
+import kotlinx.android.synthetic.main.activity_result.*
 
 class FormActivity : AppCompatActivity() {
+
+    private lateinit var preferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +24,29 @@ class FormActivity : AppCompatActivity() {
         etGasAverage.addTextChangedListener(DecimalTextWatcher(etGasAverage,1))
         etEthanolAverage.addTextChangedListener(DecimalTextWatcher(etEthanolAverage,1))
 
+        preferences = getSharedPreferences("user_preferences"   , Context.MODE_PRIVATE )
+        val fGasPrice       = preferences.getFloat("gas_price"        , 0.00f)
+        val fEthanolPrice   = preferences.getFloat("ethanol_price"    , 0.00f)
+        val fGasAverage     = preferences.getFloat("gas_average"      , 0.00f)
+        val fEthanolAverage = preferences.getFloat("ethanol_average"  , 0.00f)
+
+
+        val gasPrice = intent.extras.getDouble( "fGasPrice",  0.0 )
+        etGasPrice.setText(fGasPrice.toString())
+        etEthanolPrice.setText(fEthanolPrice.toString())
+        etGasAverage.setText(fGasAverage.toString())
+        etEthanolAverage.setText(fEthanolAverage.toString())
+
 
         btCalculate.setOnClickListener{
+
+            val editor = preferences.edit()
+            editor.putFloat("gas_price",  etGasPrice.getDouble().toFloat())
+            editor.putFloat("ethanol_price", etEthanolPrice.getDouble().toFloat())
+            editor.putFloat("gas_average", etGasAverage.getDouble().toFloat())
+            editor.putFloat("ethanol_average", etEthanolAverage.getDouble().toFloat())
+            editor.apply()
+
             val proximaTela = Intent(this, ResultActivity::class.java)
             proximaTela.putExtra("GAS_PRICE", etGasPrice.getDouble())
             proximaTela.putExtra("ETHANOL_PRICE", etEthanolPrice.getDouble())
